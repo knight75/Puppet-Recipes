f65e3f3829a3327aba78d1653ba8096609651e26
knight75 <kurgan@regie1.(none)>
2011-05-15 17:42:15 +0200

    Module Repository Class to manage special repositories first special repository is virtualbox's repository

---
+++ modules/repository/manifests/specialrepositories.pp
@@ -0,0 +1,19 @@
+
+class repository::specialrepositories {
+            aptkey { "98AB5139":
+                keyid  => "98AB5139",
+                ensure => present
+            }
+
+            file {"/etc/apt/sources.list.d/virtualbox":
+                 ensure => present,
+                 content => template("repository/virtualbox.erb"),
+                 notify => Exec["refresh_apt"],
+                 require => Aptkey[98AB5139]
+            }
+
+            exec {"refresh_apt":
+                 command => "apt-get update",
+                 subscribe   => File["/etc/apt/sources.list.d/virtualbox"] 
+            }
+}
