6a76dfc3a36997ee43df6ea4b2e7f5e2c29a94d8
knight75 <kurgan@regie1.(none)>
2011-05-15 17:40:37 +0200

    Module Repository Module to handle repositories installed on hosts Init.pp include definition of aptkeys to automatically install aptkeys

---
+++ modules/repository/manifests/init.pp
@@ -0,0 +1,37 @@
+#thanks to puppetlabs site
+#http://projects.puppetlabs.com/projects/1/wiki/Apt_Keys_Patterns
+
+define aptkey($keyid, $ensure, $keyserver = "keyserver.ubuntu.com") {
+        case $ensure {
+                present: {
+                        exec { "Import $keyid to apt keystore":
+                                environment => "HOME=/root",
+                                command     => "apt-key adv --keyserver $keyserver --recv-keys $keyid && sudo apt-get update",
+                                user        => "root",
+                                group       => "root",
+                                unless      => "apt-key list | grep $keyid",
+                                logoutput   => on_failure,
+                        }
+                }
+                absent:  {
+                        exec { "Remove $keyid from apt keystore":
+                                path    => "/bin:/usr/bin",
+                                environment => "HOME=/root",
+                                command => "apt-key del $keyid",
+                                user    => "root",
+                                group   => "root",
+                                onlyif  => "apt-key list | grep $keyid",
+                        }
+                }
+                default: {
+                        fail "Invalid 'ensure' value '$ensure' for apt::key"
+                }
+        }
+}
+
+
+class repository {
+            include repository::specialrepositories
+
+
+}
